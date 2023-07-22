87e7caaa57d97de2388d03b39ca18b2c69c16c30
Swapnil <swapnil.padaya2699@gmail.com>
2023-07-23 03:00:47 +0530

    Moved Main -> View

--- app/src/main/java/com/swapnil/myapplication/views/MainActivity.kt
+++ app/src/main/java/com/swapnil/myapplication/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.swapnil.myapplication
+package com.swapnil.myapplication.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.swapnil.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
