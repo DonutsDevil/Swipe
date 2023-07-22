4af5080ce875c7254a356a1110cea43a28531d41
Swapnil <swapnil.padaya2699@gmail.com>
2023-07-23 03:18:01 +0530

    Added Toolbar

--- app/src/main/java/com/swapnil/myapplication/views/MainActivity.kt
+++ app/src/main/java/com/swapnil/myapplication/views/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.swapnil.myapplication.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.navigateUp
 import com.swapnil.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return (navigateUp(navHostFragment.navController, (null as AppBarConfiguration?)!!)
+                || super.onSupportNavigateUp())
     }
 }
