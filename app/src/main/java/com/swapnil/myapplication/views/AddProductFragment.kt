f3b304dba6f73cb0f2e1932c75a3c2adbee6b9b6
Swapnil <swapnil.padaya2699@gmail.com>
2023-07-23 17:20:25 +0530

    Added ViewModel in AddProducts

--- app/src/main/java/com/swapnil/myapplication/views/AddProductFragment.kt
+++ app/src/main/java/com/swapnil/myapplication/views/AddProductFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.swapnil.myapplication.R
+import com.swapnil.myapplication.network.product.ProductNetworkServiceImpl
+import com.swapnil.myapplication.repository.ProductRepository
+import com.swapnil.myapplication.viewmodel.ProductViewModel
+import com.swapnil.myapplication.viewmodel.ProductViewModelFactory
 
 class AddProductFragment : Fragment() {
+
+    lateinit var productViewModel: ProductViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_product, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_product, container, false)
+        productViewModel = ViewModelProvider(
+            requireActivity(),
+            ProductViewModelFactory(ProductRepository((ProductNetworkServiceImpl())))
+        )[ProductViewModel::class.java]
+
+        return view
     }
 }
